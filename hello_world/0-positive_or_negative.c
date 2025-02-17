c82dc94dba4c24b0850832ec37a5144cf7f72822
PoussPouss <guillaume_font@orange.fr>
2025-02-17 08:46:16 +0000

    file0

--- hello_world/0-positive_or_negative.c
+++ hello_world/0-positive_or_negative.c
@@ -10,15 +10,15 @@ int main(void)
 	int n;
 if (n > 0)
 {
-printf(Ã"%d is positive\n",n);
+printf(Ã"%d is positive\n", n);
 }
 else if (n == 0)
 {
-printf("%d is zero\n",n);
+printf("%d is zero\n", n);
 }
 else
 {
-printf("%d is negative\n",n);
+printf("%d is negative\n", n);
 }
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
