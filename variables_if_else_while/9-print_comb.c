ed2412b52ced5d73c332de5a02f7e91120460a74
PoussPouss <guillaume_font@orange.fr>
2025-02-17 21:29:56 +0000

    file9

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
-
+/**
+ * main - Prints if number is positive, zero or negative
+ *
+ * Return: Always (Success)
+ */
 int main(void)
 {
-    int number = 0;
-
-    while (number < 10)
-        {
-                putchar(number + '0');
-                if (number < 9)
-                {
-                        putchar(',');
-                        putchar(' ');
-                }
-                        number++;
-                }
+int number = 0;
+	while (number < 10)
+	{
+	putchar(number + '0');
+		if (number < 9)
+	{
+		putchar(',');
+		putchar(' ');
+		}
+		number++;
+		}
 putchar('\n');
 return (0);
 }
