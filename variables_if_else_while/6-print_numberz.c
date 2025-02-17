ec25392848858c13a049923d7dacd098da5bcab9
PoussPouss <guillaume_font@orange.fr>
2025-02-17 19:47:16 +0000

    file 6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Prints if number is positive, zero or negative
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+int x;
+	for (x = 0; x < 10; x++)
+	{
+	putchar(x + '0');
+	}
+	putchar('\n');
+return (0);
+}
+
