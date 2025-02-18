b3b90b3d7b934e9c9e3439a4e2bc79d40b562bd6
PoussPouss <guillaume_font@orange.fr>
2025-02-18 14:26:23 +0000

    file 2

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,19 +1,19 @@
 #include "main.h"
 
 /**
- * print_alphabet - Prints the alphabet in lowercase
- */
+* print_alphabet_x10 - Prints the alphabet in lowercase
+*/
 
 void print_alphabet_x10(void)
 {
 int i;
 	for (i = 0; i < 10; i++)
 	{
-        char c;
-        	for (c = 'a'; c <= 'z' + 1; c++)
+char c;
+		for (c = 'a'; c <= 'z' + 1; c++)
 		{
 		_putchar(c <= 'z' ? c : '\n');
-        }
-		}
+}
+}
 
 }
