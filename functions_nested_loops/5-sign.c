f430fc7b31324a2ec568d1b003101bfcd908007f
PoussPouss <guillaume_font@orange.fr>
2025-02-19 13:29:39 +0000

    file5

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+*_printsign - checks lowercase character
+*@n: is the character to return 1 if lowercase
+*Return: 1 if c is lowercase, 0 otherwise
+*/
+int print_sign(int n)
+{
+
+	if (n > 0)
+	{
+	_putchar('+');
+	return (1);
+	}
+	else if (n == 0)
+	{
+	_putchar('0');
+	return (0);
+	}
+	else
+	{
+	_putchar('-');
+	return (-1);
+	}
+}
