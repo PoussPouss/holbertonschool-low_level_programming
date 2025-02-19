45187f8fa388dc4df3e14110158a482b22afbf8d
PoussPouss <guillaume_font@orange.fr>
2025-02-19 14:08:04 +0000

    file6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+*_abs - checks lowercase character
+*@n: is the character to return 1 if lowercase
+*Return: 1 if c is lowercase, 0 otherwise
+*/
+int _abs(int n)
+
+{
+	if (n >= 0)
+
+	return (n);
+
+	else
+
+	return (-n);
+
+}
