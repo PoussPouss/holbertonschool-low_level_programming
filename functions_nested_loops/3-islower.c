bfd53b1c60d1e8004e2ba27631ea0806462fa262
PoussPouss <guillaume_font@orange.fr>
2025-02-19 09:13:18 +0000

    file3

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,1 +1,18 @@
-test
+#include "main.h"
+
+/**
+ * int _islower for check lowercase character
+ */
+int _islower(int c) {
+
+	if (c >= 97 && c <= 122)
+	{
+
+	return 1;
+
+	}
+ 	else
+	{
+	return 0;
+	}
+	}
