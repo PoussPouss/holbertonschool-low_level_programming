d932b343fbe5994e2633f6f3ab6d8601917b06e3
PoussPouss <guillaume_font@orange.fr>
2025-02-19 22:00:07 +0000

    file 10

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- 
-add - adds two integers and returns the result
-@a: first number
-@b: second number
-Return:  the absolute value*/
+*add - adds two integers and returns the result
+*@a: first number
+*@b: second number
+*Return:  the absolute value
+*/
 
 int add(int a, int b)
 
 {
-        return (a + b);
+	return (a + b);
 }
