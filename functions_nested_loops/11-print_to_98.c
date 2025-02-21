f38e588ed32e2800b41844400fac9f3965a198a9
PoussPouss <guillaume_font@orange.fr>
2025-02-21 11:47:11 +0000

    file11

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+
+void print_to_98(int n)
+{
+int a;
+
+
+	if (n == 98)
+	{
+
+		for (a = n; a <= 98; a++)
+		{
+		printf("%d, " , a);
+		}
+
+	}
+	else (n >= 98);
+	{
+	printf("%d, \n" , a);
+
+			for (a = n; a >= 98; a--)
+			{
+				if (a != 98)
+
+				_putchar(a);
+				_putchar(',');
+				_putchar(' ');
+
+
+				else (a == 98);
+				_putchar('0');
+			}	_putchar('\n');
+
+	}
+	printf("98/n");
+}
+
