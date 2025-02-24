0b74a5c78ee733b6db22a037aaddbfe50f35d11b
PoussPouss <guillaume_font@orange.fr>
2025-02-24 09:26:01 +0000

    file0

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <ctype.h>
+/**
+*_isdigit - checks digit character
+*@c: is the character to return 1 if digit
+*Return: 0 if c is lowercase
+*/
+
+int _isdigit(int c)
+
+{
+
+	if (c >=48 && c <=57)
+	{
+	return (1);
+	}
+
+	else
+	{
+	return(0);
+	}
+
+}
