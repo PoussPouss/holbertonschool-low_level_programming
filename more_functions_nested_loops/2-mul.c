f706469d396d672d4eb9390d837c36b58fdb31a6
PoussPouss <guillaume_font@orange.fr>
2025-02-24 09:46:48 +0000

    file2

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -1,5 +1,11 @@
 #include "main.h"
 
+/**
+*mul - is a prototype
+*@a: is protduct of multiplication
+*@b: is protduct of multiplication
+*Return: result of multiplication
+*/
 int mul(int a, int b)
 {
 
