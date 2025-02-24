c0978365971c6e0c3d4f9e48f0adaae4cfcd38be
PoussPouss <guillaume_font@orange.fr>
2025-02-24 08:31:39 +0000

    file0

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,5 +1,20 @@
 #include "main.h"
 
+/**
+*_isupper - checks uppercase character
+*@c: is the character to return 1 if upper
+*Return: 1 if c is lowercase, 0 otherwise
+*/
 int _isupper(int c)
 {
+
+	if (c >= 65 && c <= 90)
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}
 
