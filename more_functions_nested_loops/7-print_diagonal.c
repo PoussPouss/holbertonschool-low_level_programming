9c6a5492aa03d63e2418f29fde08baf5599de231
PoussPouss <guillaume_font@orange.fr>
2025-02-24 16:10:13 +0000

    file7

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+*print_line - its my prototype
+*@n : variable into prototype
+*/
+
+void print_diagonal(int n)
+{
+int i = 0;
+int j;
+
+	if (n <= 0)
+	{
+	_putchar('\n');
+	return;
+	}
+
+		while (i < n)
+		{
+
+			for (j = 0; j < i; j++)
+			{
+			_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		i++;
+
+		}
+
+
+}
+
+
+
