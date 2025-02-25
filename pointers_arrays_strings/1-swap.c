8346bc5dc74165476b074452c410d55744bee3af
PoussPouss <guillaume_font@orange.fr>
2025-02-25 09:42:12 +0000

    file1

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+*swap_int - its my prototype
+*@a : variable into prototype
+*@b : variable into prototype
+*/
+
+void swap_int(int *a, int *b)
+{
+
+int temp = *a;
+*a = *b;
+*b = temp;
+
+}
