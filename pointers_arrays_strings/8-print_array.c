d0e2095f1992bb137a6c7aad3d60345f03056ef3
PoussPouss <guillaume_font@orange.fr>
2025-02-26 23:12:05 +0100

    file 8

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -1,1 +1,24 @@
 #include "main.h"
+#include <stdio.h>
+/**
+ *print_array - prototype string character
+ *@a: pointeurs variable
+ *@b: variable
+ */
+void print_array(int *a, int n)
+
+{
+    int i;
+
+    for (i = 0; i < n; i++)
+    {
+        if (i != n - 1)
+        printf ("%d, ", a[i]);
+
+        else
+        printf("%d", a[i]);
+    }
+    printf("\n");
+
+
+}
