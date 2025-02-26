f3fe210b85c608da97a1ff7d36ebbfe9fd3a4802
PoussPouss <guillaume_font@orange.fr>
2025-02-26 14:16:50 +0100

    file 6

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -9,10 +9,10 @@ void puts2(char *str)
 if (str == NULL)
 	return;
 
-for (i = 0; str[i] != '\0'; i += 2)
-{
+	for (i = 0; str[i] != '\0'; i += 2)
+	{
 	_putchar(str[i]);
-}
-_putchar('\n');
+	}
+	_putchar('\n');
 
 }
