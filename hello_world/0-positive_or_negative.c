9ddc516882086e2cfd4842864b34a6b77e3e6b42
PoussPouss <guillaume_font@orange.fr>
2025-02-26 09:44:13 +0100

    "Change file 0"

--- hello_world/0-positive_or_negative.c
+++ hello_world/0-positive_or_negative.c
@@ -1,27 +1,43 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+
 /**
- * main - more headers goes there betty style doc for function main goes there
- * Return: intger
+ * main - Prints if number is positive, zero or negative
+ *
+ * Return: Always (Success)
  */
 int main(void)
 {
 	int n;
-if (n > 0)
-{
-printf("%d is positive\n", n);
-}
-else if (n == 0)
-{
-printf("%d is zero\n", n);
-}
-else
-{
-printf("%d is negative\n", n);
-}
+
+	if (n > 0)
+		{
+		printf("is positive");
+		}
+	else if (n == 0)
+		{
+		printf("is zero");
+		}
+	else if (n < 0)
+		{
+		printf("is negative");
+		}
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
+
+	if (n % 10 > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
+	}
+	else if (n % 10 < 6 && n != 0)
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
+	}
+	else if (n % 10 == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, n % 10);
+	}
 	return (0);
-}
+	}
