9a22d97a146936e2fedb1d3dc2ba10a1a2109048
PoussPouss <guillaume_font@orange.fr>
2025-02-26 22:10:31 +0100

    file 7

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -1,1 +1,25 @@
 #include "main.h"
+/**
+ *puts_half - prototype string character
+ *@str: pointeurs variable
+ */
+void puts_half(char *str)
+
+{
+int length = 0;
+int n;
+int i;
+while (str[length] != '\0')
+{
+length++;
+}
+
+n = (length + 1) / 2;
+
+for (i = n; str[i] != '\0'; i++)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
+
+}
