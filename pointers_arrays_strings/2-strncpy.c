7e47064ecf412355f356a6aa5ff5411dc92b47e9
PoussPouss <guillaume_font@orange.fr>
2025-02-27 23:16:23 +0100

    file 2

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+*_strncpy - prototype string character
+*@dest: pointeurs variable
+*@src: variable
+*@n: variable
+*Return: initial dest
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+int i = 0;
+
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+while (i < n)
+{
+dest[i] =  '\0';
+i++;
+}
+
+return (dest);
+}
