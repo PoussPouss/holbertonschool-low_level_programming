fa89aac60706544fb895663c17e19b98dbbd8330
PoussPouss <guillaume_font@orange.fr>
2025-02-28 16:38:59 +0100

    file 6

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * cap_string - Capitalise la première lettre de chaque mot d'une chaîne.
  * @str: La chaîne à traiter.
@@ -7,50 +6,44 @@
  */
 char *cap_string(char *str)
 {
-    int i;
-    int in_word = 0;
-    int j;
-    char separators[] = {',', ';', '.', '!', '?', '"', '(', ')', '{', '}', ' ', '\t', '\n'};
+int i;
+int in_word = 0;
+int j;
+int is_separator;
+char separators[] = {',', ';', '.', '!', '?', '"', '
+(', ')', '{', '}', ' ', '\t', '\n'};
 
-    
-    if (str[0] >= 'a' && str[0] <= 'z')
-    {
-        str[0] = str[0] - 32;
-    }
-
-    
-    for (i = 1; str[i] != '\0'; i++)
-    {
-        int is_separator = 0; 
-
-        
-        for (j = 0; separators[j] != '\0'; j++)
-        {
-            if (str[i] == separators[j])
-            {
-                is_separator = 1;
-                break;
-            }
-        }
-
-        
-        if (is_separator)
-        {
-            in_word = 0;
-        }
-        else if (str[i] >= 'a' && str[i] <= 'z' && in_word == 0)
-        {
-        
-            str[i] = str[i] - 32;
-            in_word = 1;
-        }
-        else
-        {
-            in_word = 1;
-        }
-    }
-
-    return str;
+if (str[0] >= 'a' && str[0] <= 'z')
+	{
+str[0] = str[0] - 32;
 }
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		is_separator = 0;
 
+	for (j = 0; separators[j] != '\0'; j++)
+	{
+		if (str[i] == separators[j])
+		{
+		is_separator = 1;
+		break;
+		}
+	}
 
+	if (is_separator)
+	{
+	in_word = 0;
+	}
+	else if (str[i] >= 'a' && str[i] <= 'z' && in_word == 0)
+	{
+		str[i] = str[i] - 32;
+	in_word = 1;
+	}
+	else
+	{
+	in_word = 1;
+	}
+	}
+
+return (str);
+}
