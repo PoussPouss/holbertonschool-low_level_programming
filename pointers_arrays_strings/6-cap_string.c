07a7dd1e0574fed8b9e6e7feca9282dca6b3261e
PoussPouss <guillaume_font@orange.fr>
2025-02-28 15:13:29 +0100

    file 6

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,32 +1,46 @@
 #include "main.h"
-#include <stdio.h>
-#include <string.h>
-
 /**
-*cap_string - prototype string character
-*@str: variable
+* cap_string - Capitalise la première lettre de chaque mot d'une chaîne.
+* @str: La chaîne à traiter.
+* Return: La chaîne modifiée.
 */
 char *cap_string(char *str)
-
 {
 int i;
-char separator[] = " \t\n,;.!?\"(){}";
 
-if (str[0] >= 'a' && str[0] <= 'z')
-{
-str[0] = str[0] - 32;
-}
-for (i = 0; str[i] != '\0'; i++)
-{
-	if (strchr(separator, str[i]))
+int in_word = 0;
+
+int is_separator = 0;
+
+int j;
+
+char separators[] = " \t\n,;.!?\"(){}";
+
+	if (str[0] >= 'a' && str[0] <= 'z')
 	{
-		if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
-		{
-			str[i + 1] = str[i + 1] - 32;
-		}
+		str[0] = str[0] - 32;
 	}
 
-}
-return (str);
+	for (i = 1; str[i] != '\0'; i++)
 
+		for (j = 0; separators[j] != '\0'; j++)
+		{
+			if (str[i] == separators[j])
+			{
+				is_separator = 1;
+				break;
+			}
+		}
+
+			if (is_separator)
+			in_word = 0;
+
+		else if (str[i] >= 'a' && str[i] <= 'z' && in_word == 0)
+		in_word = 1;
+		else
+		in_word = 1;
+
+
+	return (str);
 }
+
