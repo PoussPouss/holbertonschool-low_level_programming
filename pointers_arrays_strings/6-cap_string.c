d278b80cb1426eda7af4ec95406d595079add7da
PoussPouss <guillaume_font@orange.fr>
2025-02-28 21:51:01 +0100

    file 6

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,41 +1,46 @@
 #include "main.h"
+/**
+*cap_string - prototype string character
+*@str: variable
+*Return: initial dest
+*/
 
 char *cap_string(char *str)
 {
-    int i = 0;
-    char separators[] = " \t\n,;.!?\"(){}";
-    int is_start_of_word = 1;
+	int i = 0;
+	char separators[] = " \t\n,;.!?\"(){}";
+	int is_start_of_word = 1;
 
-    while (str[i] != '\0')
-    {
-        int j = 0;
-        int is_separator = 0;
+	while (str[i] != '\0')
+	{
+		int j = 0;
+		int is_separator = 0;
 
-        while (separators[j] != '\0')
-        {
-            if (str[i] == separators[j])
-            {
-                is_separator = 1;
-                break;
-            }
-            j++;
-        }
+		while (separators[j] != '\0')
+		{
+			if (str[i] == separators[j])
+			{
+				is_separator = 1;
+				break;
+			}
+			j++;
+		}
 
-        if (is_start_of_word && (str[i] >= 'a' && str[i] <= 'z'))
-        {
-            str[i] -= 'a' - 'A';
-        }
+		if (is_start_of_word && (str[i] >= 'a' && str[i] <= 'z'))
+		{
+			str[i] -= 'a' - 'A';
+		}
 
-        if (is_separator)
-        {
-            is_start_of_word = 1;
-        }
-        else
-        {
-            is_start_of_word = 0;
-        }
+		if (is_separator)
+		{
+			is_start_of_word = 1;
+		}
+		else
+		{
+			is_start_of_word = 0;
+		}
 
-        i++;
-    }
-    return str;
+		i++;
+	}
+	return (str);
 }
