d768d339027eb5c1077e934a0f10a38bd3723d4c
PoussPouss <guillaume_font@orange.fr>
2025-03-03 17:11:59 +0100

    file 5

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -1,8 +1,31 @@
 #include "main.h"
 #include <stdio.h>
-
+/**
+*_strstr - prototype contenant mes chaines de caractères
+*@haystack: première chaine de caractère
+*@needle: chaine a trouver dans haystack
+*Return: value of my needle string
+ */
 char *_strstr(char *haystack, char *needle)
-{
 
-    
+{
+	int i = 0;
+	int j;
+
+	while (haystack[i] != '\0')
+	{
+	j = 0;
+		if (haystack[i] == needle[j])
+		{
+			while (needle[j] != haystack[i])
+			{
+				if (haystack[i] == needle[j])
+				j++;
+			}
+			return (haystack + i);
+		}
+		i++;
+	}
+	return (NULL);
+
 }
