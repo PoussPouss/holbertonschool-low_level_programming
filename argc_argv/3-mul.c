d55475a4062175cfcdf0ce6b44613029e455a0f3
PoussPouss <guillaume_font@orange.fr>
2025-03-05 15:51:09 +0100

    betty all

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,23 +1,37 @@
 #include <stdlib.h>
 #include <stdio.h>
+/**
+* main - Multiplies two numbers
+*
+* This program takes two arguments, converts them to integers,
+* multiplies them, and prints the result followed by a new line.
+* If the program does not receive exactly two arguments,
+* it prints "Error" and returns 1.
+*
+* @argc: Argument count (number of command-line arguments)
+* @argv: Argument vector (array of strings containing the arguments)
+*
+* Return: 0 on success, 1 on error
+*/
+
 
 int main(int argc, char *argv[])
 {
-    int i;
-    int j;
-    int result;
+	int i;
+	int j;
+	int result;
 
-    if (argc != 3)
-    {
-    printf("Error\n");
-    return (1);
-    }
+	if (argc != 3)
+	{
+	printf("Error\n");
+	return (1);
+	}
 
-    i = atoi(argv[1]);
-    j = atoi(argv[2]);
-    result = i * j;
+	i = atoi(argv[1]);
+	j = atoi(argv[2]);
+	result = i * j;
 
-    printf("%d\n", result);
+	printf("%d\n", result);
 
 	return (0);
 }
--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -1,38 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+/**
+* main - Adds positive numbers
+*
+* This program takes multiple arguments, checks if they
+* are valid positive integers, adds them together, and
+* prints the result followed by a new line.
+* If no arguments are provided, it prints 0.
+* If an argument contains a non-digit character,
+* it prints "Error" and returns 1.
+*
+* @argc: Argument count (number of command-line arguments)
+* @argv: Argument vector (array of strings containing the arguments)
+*
+* Return: 0 on success, 1 on error
+*/
 
 
 int main(int argc, char *argv[])
 {
 int num;
-int i;
-int j;
-int result= 0;
+int i = 0;
+int j = 0;
+int result = 0;
 
-if (argc == 1)
-{
-printf("0\n");
-return (0);
-}
-if (argc > 1)
-{
+	if (argc == 1)
+	{
+	printf("0\n");
+	return (0);
+	}
 
-for (i = 1; i < argc; i++)
-{
-    for (j = 0; argv[i][j] != '\0'; j++)
-    {
-        if (!isdigit(argv[i][j]))
-        printf("Error\n");
-        
-    }
-    return (1);
+	for (i = 1; i < argc; i++)
+	{
+	for (j = 0; argv[i][j] != '\0'; j++)
+	{
+	if (!isdigit(argv[i][j]))
+		{
+		printf("Error\n");
+		return (1);
+		}
+	}
 num = atoi(argv[i]);
+
 result += num;
 
 }
-printf("%d\n", result);
 
-}
+printf("%d\n", result);
 return (0);
 }
