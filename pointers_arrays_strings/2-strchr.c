75efbbe4fa2894367b8d39ce43dce4237df3cd71
PoussPouss <guillaume_font@orange.fr>
2025-03-06 09:13:46 +0100

    file 2

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -12,10 +12,7 @@ char *_strchr(char *s, char c)
 {
 int i = 0;
 
-if (c == '\0')
-{
-return (0);
-}
+
 
 while (s[i] != '\0')
 {
@@ -23,6 +20,11 @@ while (s[i] != '\0')
 if (s[i] == c)
 return (s + i);
 i++;
+}
+
+if (c == '\0')
+{
+return (s);
 }
 
 return (NULL);
