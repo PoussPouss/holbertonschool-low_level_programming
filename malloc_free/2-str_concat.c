7843ddef45c6f3e73318f4953a7af4e94e470abb
PoussPouss <guillaume_font@orange.fr>
2025-03-10 14:17:41 +0100

    file 2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -12,6 +12,10 @@
 char *str_concat(char *s1, char *s2)
 {
 char *concat;
+int i;
+int j;
+int len1 = 0;
+int len2 = 0;
 
 if (s1 == NULL)
 {
@@ -23,10 +27,23 @@ if (s2 == NULL)
 s2 = "";
 }
 
-concat = malloc(strlen(s1) + strlen(s2) + 1);
-strcpy(concat, s1);
-strcat(concat, s2);
+while (s1[len1] != '\0')
+len1++;
+
+while (s2[len2] != '\0')
+len2++;
+
+concat = malloc((len1 + len2 + 1) * sizeof(char));
+if (concat == NULL)
+return (NULL);
+
+for (i = 0; i < len1; i++)
+concat[i] = s1[i];
+
+for (j = 0; j < len2; j++)
+concat[i + j] = s2[j];
+
+concat [i + j] = '\0';
 
 return (concat);
-
 }
