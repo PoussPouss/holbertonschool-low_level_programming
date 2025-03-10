a61b9e431117656dbe0c5a43207846436a9a7fff
PoussPouss <guillaume_font@orange.fr>
2025-03-10 15:41:23 +0100

    file 3

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -1,17 +1,51 @@
 #include "main.h"
-#include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
-
+/**
+*alloc_grid - Creates an array of chars and initializes it with a char
+*@width: The size of the array
+*@height: Size of second array
+* Return: Pointer to the array, or NULL if it fails
+*/
 int **alloc_grid(int width, int height)
 {
-    int *alloc;
+int **alloc;
+int i;
+int j;
 
-    if (width == 0)
-    return (NULL);
+	if (width <= 0)
+	return (NULL);
 
-    if (height == 0)
-    return (NULL);
+	if (height <= 0)
+	return (NULL);
 
+alloc = malloc (height * sizeof(int *));
 
+	if (alloc == NULL)
+	return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+	alloc[i] = malloc(width * sizeof(int));
+
+	if (alloc[i] == NULL)
+	{
+		for (j = 0; j < i; j++)
+		{
+			free(alloc[j]);
+		}
+
+		free(alloc);
+		return (NULL);
+	}
+
+}
+
+for (i = 0; i < height; i++)
+{
+	for (j = 0; j < width; j++)
+	{
+	alloc[i][j] = 0;
+	}
+}
+return (alloc);
 }
