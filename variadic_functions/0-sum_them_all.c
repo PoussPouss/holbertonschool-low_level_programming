70e4adef385704042d0f3ae8a217a26ebfa0d8b3
PoussPouss <guillaume_font@orange.fr>
2025-03-18 14:46:57 +0100

    file 0 changement

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -12,11 +12,13 @@
 int sum_them_all(const unsigned int n, ...)
 {
 
+va_list args;
+
 unsigned int i;
 
 unsigned int sum = 0;
 
-va_list args;
+
 
 	if (n == 0)
 	return (0);
@@ -24,8 +26,10 @@ va_list args;
 	va_start(args, n);
 
 for (i = 0; i < n; i++)
+{
+	sum += va_arg(args, int);
+}
 
-sum += va_arg(args, int);
 return (sum);
 va_end(args);
 }
