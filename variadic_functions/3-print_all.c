1e81f0858e2a0372c596f165d537d7d5a30d13f8
PoussPouss <guillaume_font@orange.fr>
2025-03-19 14:59:47 +0100

    Ajout de fonctions d'impression pour différents types d'arguments dans print_all

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,64 +1,97 @@
 #include "variadic_functions.h"
 #include <stdio.h>
 #include <stdarg.h>
+
 /**
- * print_all - Imprime des arguments de différents types
- * @format: Chaîne de caractères indiquant les types d'arguments
- *          c: char
- *          i: integer
- *          f: float
- *          s: char * (si la chaîne est NULL, imprime (nil))
- * @...: Liste variable des arguments à imprimer
- *
- * Description: Les arguments sont imprimés selon le format spécifié
- * et séparés par des virgules
- * Return: Void
- */
+*print_char - imprime un caractère.
+*@args: Liste des arguments variadiques
+*/
 
+void print_char(va_list args)
+{
+	printf("%c", va_arg(args, int));
+}
+
+/**
+*print_int - imprime un entier.
+*@args: Liste des arguments variadiques
+*/
+
+void print_int(va_list args)
+{
+	printf("%d", va_arg(args, int));
+}
+
+/**
+* print_float - imprime un nombre à virgule flottante.
+* @args: Liste des arguments variadiques
+*/
+void print_float(va_list args)
+{
+	printf("%f", va_arg(args, double));
+}
+
+/**
+* print_string - imprime une chaîne de caractères.
+* @args: Liste des arguments variadiques
+*/
+void print_string(va_list args)
+{
+	char *str = va_arg(args, char*);
+
+	if (str == NULL)
+		str = "(nil)";
+	printf("%s", str);
+}
+
+/**
+* struct print_func - Structure defining a function based on format character
+* @type: The format character
+* @print_func: The function to call for printing
+*/
+typedef struct print_func
+{
+	char type;
+
+	void (*print_func)(va_list);
+} print_func_t;
+
+/**
+* print_all - prints anything based on format
+* @format: list of types of arguments passed to the function
+*/
 void print_all(const char * const format, ...)
-
 {
 	va_list args;
 	int i = 0;
-
-	char *str;
-
+	int j = 0;
 	int first = 1;
+	print_func_t print_funcs[] = {
+		{'c', print_char},
+		{'i', print_int},
+		{'f', print_float},
+		{'s', print_string},
+	};
 
 	va_start(args, format);
 
-	if (format == NULL)
-	return;
-
-	while (format[i])
+	while (format && format[i])
 	{
-		if (first == 0)
-			printf(", ");
-
-		if (format[i] == 'c')
+		j = 0;
+		while (j < 4)
 		{
-			printf("%c", va_arg(args, int));
-		}
-		else if (format[i] == 'i')
-		{
-			printf("%d", va_arg(args, int));
-		}
-		else if (format[i] == 'f')
-		{
-			printf("%f", va_arg(args, double));
-		}
-		else if (format[i] == 's')
-		{
-			str = va_arg(args, char *);
-			printf("%s", str ? str : "(nil)");
-		}
-
-		{
-		first = 0;
+			if (format[i] == print_funcs[j].type)
+			{
+				if (first == 0)
+					printf(", ");
+				first = 0;
+				print_funcs[j].print_func(args);
+				break;
+			}
+			j++;
 		}
 		i++;
 	}
-		va_end(args);
-		printf("\n");
-
+	printf("\n");
+	va_end(args);
 }
