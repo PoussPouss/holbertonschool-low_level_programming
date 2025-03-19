20771373aa9c625b0ea01677008cddae0c8901d8
PoussPouss <guillaume_font@orange.fr>
2025-03-19 09:36:29 +0100

    Correction de la fonction print_strings pour gérer les chaînes de caractères et afficher (nil) pour les pointeurs nuls

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -3,27 +3,34 @@
 #include <stdarg.h>
 
 /**
- * print_strings - Prints numbers followed by a new line
- * @separator: The string to print between numbers
- * @n: The number of integers passed to the function
- *
- * Return: Nothing
- */
+* print_strings - Prints numbers followed by a new line
+* @separator: The string to print between numbers
+* @n: The characters passed to the function
+*
+* Return: Nothing
+*/
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list args;
 	unsigned int i;
+	char *str;
 
 	va_start(args, n);
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%d", va_arg(args, int));
 
+	str = va_arg(args, char *);
+
+	if (str == NULL)
+	{
+	printf("(nil)");
+	}
+	else
+	printf("%s", str);
 	if (separator != NULL && i < n - 1)
 		printf("%s", separator);
 	}
-
 	printf("\n");
 	va_end(args);
 
