aa14958bb2d287879f50aaca611a77d98854e960
PoussPouss <guillaume_font@orange.fr>
2025-04-01 15:03:32 +0200

    Correction de la fonction insert_dnodeint_at_index pour gérer les cas d'insertion à l'index 0 et à la fin de la liste, ajout de vérifications de NULL et amélioration de la gestion de la mémoire.

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <stdio.h>
 #include "lists.h"
+
 /**
  * insert_dnodeint_at_index - Insère un nouveau nœud à une position donnée
  * @h: Pointeur vers le pointeur de la tête de la liste
@@ -9,45 +10,39 @@
  * @n: Valeur à stocker dans le nouveau nœud
  * Return: Adresse du nouveau nœud, ou NULL si l'opération a échoué
  */
-
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *current = *h;
-	unsigned int i;
-
-	dlistint_t *new = malloc(sizeof(dlistint_t));
-
-	if (new == NULL)
-	return (NULL);
-
-	new->n = n;
+	dlistint_t *current, *new_node;
+	unsigned int i = 0;
 
 	if (idx == 0)
-	{
-		new->next = *h;
-		new->prev = NULL;
+		return (add_dnodeint(h, n));
 
-		if (*h != NULL)
-		(*h)->prev = new;
-	*h = new;
-	return (new);
-	}
+	if (h == NULL)
+		return (NULL);
 
-	for (i = 0; current != NULL && i < idx; i++)
+	current = *h;
+	while (current != NULL && i < idx - 1)
 	{
 		current = current->next;
+		i++;
 	}
+
 	if (current == NULL)
-	{
-		free(new);
 		return (NULL);
-	}
-	new->next = current;
-	new->prev = current->prev;
 
-	if (current->prev != NULL)
-	current->prev->next = new;
-current->prev = new;
+	if (current->next == NULL)
+		return (add_dnodeint_end(h, n));
 
-return (new);
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->prev = current;
+	new_node->next = current->next;
+	current->next->prev = new_node;
+	current->next = new_node;
+
+	return (new_node);
 }
