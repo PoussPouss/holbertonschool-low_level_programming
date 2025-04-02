9778fc4fb0edd22cf4abc2a623f0d4003c63f737
PoussPouss <guillaume_font@orange.fr>
2025-04-02 14:48:18 +0200

    Correction de la déclaration de write_status en ssize_t et réorganisation des flags d'ouverture de fichier dans la fonction create_file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -17,14 +17,14 @@ int create_file(const char *filename, char *text_content)
 {
 	int file_descriptor;
 
-	int write_status;
+	ssize_t write_status;
 
 	int text_length = 0;
 
 	if (filename == NULL)
 	return (-1);
 
-	file_descriptor = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	file_descriptor = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (file_descriptor == -1)
 	return (-1);
 
