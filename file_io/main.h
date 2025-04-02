98ed42584b4e58a7ee1f5797e65507523b367e17
PoussPouss <guillaume_font@orange.fr>
2025-04-02 14:56:33 +0200

    Ajout des inclusions nécessaires pour la gestion des fichiers dans main.h

--- file_io/main.h
+++ file_io/main.h
@@ -2,7 +2,12 @@
 #define MAIN_H
 
 #include <stddef.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
 #include <sys/types.h>
+#include <sys/stat.h>
+#include <stdlib.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 
