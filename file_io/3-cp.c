cee3f638e90dcf027b865baa06b2d732990334b9
PoussPouss <guillaume_font@orange.fr>
2025-04-03 14:03:29 +0200

    Amélioration de la gestion des erreurs lors de la fermeture des fichiers : ajout de messages d'erreur explicites et sortie en cas d'échec.

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -83,10 +83,16 @@ void copy_content(int file_from, int file_to, char *argv[])
 void close_files(int file_from, int file_to, char *argv[])
 {
 	if (close(file_from) == -1)
-		error_file(file_from, -1, argv, 100);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
 
 	if (close(file_to) == -1)
-		error_file(file_to, -1, argv, 100);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
+		exit(100);
+	}
 }
 
 /**
