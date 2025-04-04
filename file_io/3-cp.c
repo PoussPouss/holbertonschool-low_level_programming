aaf8b4ea6043ec0f40acf8759bcd2cd7b01cfef4
PoussPouss <guillaume_font@orange.fr>
2025-04-04 10:35:21 +0200

    Refactor la gestion des erreurs : introduction de la fonction close_checked pour fermer les descripteurs de fichiers avec vérification et ajout de la fonction check_args pour valider les arguments d'entrée.

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -4,23 +4,41 @@
 #include <fcntl.h>
 
 /**
- * error_handler - Gère les erreurs et quitte le programme
- * @code: Code d'erreur (97, 98, 99, 100)
- * @message: Message d'erreur
- * @fn: Nom de fichier ou valeur FD à afficher
- * @fd_from: Descripteur du fichier source à fermer si != -1
- * @fd_to: Descripteur du fichier destination à fermer si != -1
+ * close_checked - Ferme un descripteur de fichier avec vérification
+ * @fd: Descripteur à fermer
  */
-void error_handler(int code, char *message, char *fn, int fd_from, int fd_to)
+void close_checked(int fd)
 {
-	dprintf(STDERR_FILENO, "%s%s\n", message, fn);
+	if (close(fd) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
 
-	if (fd_from != -1)
-		close(fd_from);
-	if (fd_to != -1)
-		close(fd_to);
+/**
+ * check_args - Vérifie les arguments et ouvre les fichiers
+ * @argc: Nombre d'arguments
+ * @argv: Tableau des arguments
+ * @fd_from: Pointeur vers le descripteur du fichier source
+ * @fd_to: Pointeur vers le descripteur du fichier destination
+ */
+void check_args(int argc, char *argv[], int *fd_from, int *fd_to)
+{
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
 
-	exit(code);
+	*fd_from = open(argv[1], O_RDONLY);
+	if (*fd_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+
+	*fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 }
 
 /**
@@ -35,41 +53,30 @@ int main(int argc, char *argv[])
 	int fd_from = -1, fd_to = -1;
 	ssize_t n_read, n_write;
 	char buffer[1024];
-	char fd_str[20];
 
-	/* Vérification des arguments */
-	if (argc != 3)
-		error_handler(97, "Usage: cp file_from file_to", "", -1, -1);
+	check_args(argc, argv, &fd_from, &fd_to);
 
-	/* Ouverture du fichier source */
-	fd_from = open(argv[1], O_RDONLY);
-	if (fd_from == -1)
-		error_handler(98, "Error: Can't read from file ", argv[1], -1, -1);
-
-	/* Ouverture/création du fichier destination */
-	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (fd_to == -1)
-		error_handler(99, "Error: Can't write to ", argv[2], fd_from, -1);
-	/* Copie du contenu */
 	while ((n_read = read(fd_from, buffer, 1024)) > 0)
 	{
 		n_write = write(fd_to, buffer, n_read);
-		if (n_write == -1 || n_write != n_read)
-			error_handler(99, "Error: Can't write to ", argv[2], fd_from, fd_to);
+		if (fd_to == -1 || n_write != n_read)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			close(fd_from);
+			exit(99);
+		}
 	}
-	/* Erreur de lecture */
+
 	if (n_read == -1)
-		error_handler(98, "Error: Can't read from file ", argv[1], fd_from, fd_to);
-	/* Fermeture des fichiers */
-	if (close(fd_from) == -1)
 	{
-		sprintf(fd_str, "%d", fd_from);
-		error_handler(100, "Error: Can't close fd ", fd_str, -1, fd_to);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		close(fd_from);
+		if (fd_to != -1)
+			close(fd_to);
+		exit(98);
 	}
-	if (close(fd_to) == -1)
-	{
-		sprintf(fd_str, "%d", fd_to);
-		error_handler(100, "Error: Can't close fd ", fd_str, -1, -1);
-	}
+
+	close_checked(fd_from);
+	close_checked(fd_to);
 	return (0);
 }
